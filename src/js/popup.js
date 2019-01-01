597b2721729c99ddb35f0ec7d7a9cc81c99d0c77
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2019-01-01 12:16:14 -0500

    Initialized boilerplate, created simple (ugly) UI

---
+++ src/js/popup.js
@@ -0,0 +1,25 @@
+import "../css/popup.css";
+var $ = require('jQuery');
+
+/*
+  Adding a blocked site
+*/
+$(".input").change(function(){
+  $('.blocked-list').append('<li>' + $(".input").val() + '</li>')
+
+  //  Validate the domain input is correct
+  //  ""
+  //  ""
+  //  ""
+
+  //  Clear the input field
+  //  $(".input").val('')
+
+  //  Establish connection with extension to send messages
+  // var port = chrome.extension.connect({
+    // name: "Content script"
+  // })
+
+  //  Pass the blocked domain to extension
+  // port.postMessage($(".input").val());
+});
