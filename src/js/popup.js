aa1410f9f7b5902c05ac19bd20e14858db902cd9
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2019-01-02 16:00:22 -0500

    Store blocked urls in chrome local storage

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,33 +2,48 @@ import "../css/popup.css";
 var $ = require('jQuery');
 
 /*
-  Adding a blocked site
+  Use Chrome background page to store HTML session
 */
-$(".input").change(function(){
-  $('.blocked-list').append('<li class="blocked-item"><span class="blocked-item--website">' + $(".input").val() + '</span>')
-  $('.blocked-list').append('<span class="blocked-item--delete">X</span></li>')
-  // $('.blocked-list').append('<i class="blocked-item--delete fas fa-times" onclick=""></i></li>')
+chrome.runtime.getBackgroundPage(bg => {
+  //  Get previous session
+  if(bg.sessionDataHTML){
+    document.body.innerHTML = bg.sessionDataHTML;
+  }
+  //  Update the session to include changes
+  setInterval(function(){
+    bg.sessionDataHTML = document.body.innerHTML
+  },1000);
 
-  //  Validate the domain input is correct
-  //  ""
-  //  ""
-  //  ""
+  /*
+    Adding a blocked site
+  */
+  $(".input").change(() => {
+    $('.blocked-list').append('<li class="blocked-item"><span class="blocked-item--website">' + $(".input").val() + '</span>')
+    $('.blocked-list').append('<span class="blocked-item--delete">X</span></li>')
+    // $('.blocked-list').append('<i class="blocked-item--delete fas fa-times" onclick=""></i></li>')
 
-  //  Clear the input field
-  //  $(".input").val('')
+    //  Validate the domain input is correct
+    //  ""
+    //  ""
+    //  ""
 
-  //  Establish connection with extension to send messages
-  var port = chrome.extension.connect({
-    name: "Content script"
+    //  Clear the input field
+    //  $(".input").val('')
+
+    //  Establish connection with extension to send messages
+    var port = chrome.extension.connect({
+      name: "Content script"
+    })
+
+    //  Pass the blocked domain to extension
+    port.postMessage($(".input").val());
+  });
+
+  /*
+    Removing a blocked site
+  */
+  $(".blocked-item--delete").click(function() {
+    console.log("item is being deleted...")
   })
 
-  //  Pass the blocked domain to extension
-  port.postMessage($(".input").val());
-});
-
-/*
-  Removing a blocked site
-*/
-$(".blocked-item--delete").click(function() {
-  console.log("item is being deleted...")
 })
